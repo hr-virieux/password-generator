d972646aa7891e022dacbb3664daa3417b7cdfd7
Henry Virieux <105490926+hr-virieux@users.noreply.github.com>
2023-12-14 22:05:52 +1100

    Update script.js
    JavaScript code added

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Function to generate a random password
+function generatePassword() {
+    var passwordLength;
+    var includeLowercase, includeUppercase, includeNumeric, includeSpecial;
+    var possibleCharacters = '';
+
+    // Password length
+    passwordLength = parseInt(prompt("Enter the password length (8-128 characters):"));
+    while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+        alert("Invalid length. Please enter a number between 8 and 128.");
+        passwordLength = parseInt(prompt("Enter the password length (8-128 characters):"));
+    }
+
+    // Characters to include options
+    includeLowercase = confirm("Include lowercase letters?");
+    includeUppercase = confirm("Include uppercase letters?");
+    includeNumeric = confirm("Include numeric characters?");
+    includeSpecial = confirm("Include special characters?");
+
+    // Characters available to add
+    if (includeLowercase) possibleCharacters += 'abcdefghijklmnopqrstuvwxyz';
+    if (includeUppercase) possibleCharacters += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if (includeNumeric) possibleCharacters += '0123456789';
+    if (includeSpecial) possibleCharacters += '!@#$%^&*()_+';
+
+    // Generate the password
+    var password = '';
+    for (var i = 0; i < passwordLength; i++) {
+        var randomIndex = Math.floor(Math.random() * possibleCharacters.length);
+        password += possibleCharacters[randomIndex];
+    }
+    return password;
+}
+
+// Password output
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
